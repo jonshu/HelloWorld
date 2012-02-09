e4e240ccde55c5647e90839430ddfc21f0c6dd2c
Jonathan Shu <jonshu@gmail.com>
2012-02-09 10:52:43 -0800

    *initial

--- Hello.java
+++ Hello.java
@@ -4,6 +4,9 @@ public class Hello {
         System.out.println("Hello, World");
         System.out.println("Second");
         System.out.println("third");
+
+        System.out.println("fourth");
+
         
     }
 
